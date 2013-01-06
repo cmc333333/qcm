3e2f953fec92844ea168b27cc0165c99dddb33f1
CM Lubinski <cmc@agile>
2013-01-06 17:58:34 -0600

    Creates the db

---
+++ init.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+conn = sqlite3.connect('qcm.db')
+
+conn.execute("""
+    CREATE TABLE IF NOT EXISTS activities (
+      key INTEGER PRIMARY KEY,
+      value TEXT  --  JSON
+    );
+    """)
+conn.execute("""
+    CREATE TABLE IF NOT EXISTS metric (
+      key INTEGER PRIMARY KEY,
+      value TEXT  --  JSON
+    );
+    """)
